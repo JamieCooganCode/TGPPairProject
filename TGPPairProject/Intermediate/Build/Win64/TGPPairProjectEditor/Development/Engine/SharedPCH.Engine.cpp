f57b0bbeaabcf2e47faa1c9e094f4a484701a7d6
OverAHill <33007028+OverAHill@users.noreply.github.com>
2018-11-08 10:15:13 +0000

    Initial Commit
    Basic C++ empty Project

---
+++ TGPPairProject/Intermediate/Build/Win64/TGPPairProjectEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/h017026h/TGPPairProject/TGPPairProject/Intermediate/Build/Win64/TGPPairProjectEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ TGPPairProject/Intermediate/Build/Win64/UE4Editor/Inc/TGPPairProject/TGPPairProjectGameModeBase.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef TGPPAIRPROJECT_TGPPairProjectGameModeBase_generated_h
+#error "TGPPairProjectGameModeBase.generated.h already included, missing '#pragma once' in TGPPairProjectGameModeBase.h"
+#endif
+#define TGPPAIRPROJECT_TGPPairProjectGameModeBase_generated_h
+
+#define TGPPairProject_Source_TGPPairProject_TGPPairProjectGameModeBase_h_15_RPC_WRAPPERS
+#define TGPPairProject_Source_TGPPairProject_TGPPairProjectGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define TGPPairProject_Source_TGPPairProject_TGPPairProjectGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesATGPPairProjectGameModeBase(); \
+	friend struct Z_Construct_UClass_ATGPPairProjectGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ATGPPairProjectGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/TGPPairProject"), NO_API) \
+	DECLARE_SERIALIZER(ATGPPairProjectGameModeBase)
+
+
+#define TGPPairProject_Source_TGPPairProject_TGPPairProjectGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesATGPPairProjectGameModeBase(); \
+	friend struct Z_Construct_UClass_ATGPPairProjectGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ATGPPairProjectGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/TGPPairProject"), NO_API) \
+	DECLARE_SERIALIZER(ATGPPairProjectGameModeBase)
+
+
+#define TGPPairProject_Source_TGPPairProject_TGPPairProjectGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATGPPairProjectGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATGPPairProjectGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATGPPairProjectGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATGPPairProjectGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATGPPairProjectGameModeBase(ATGPPairProjectGameModeBase&&); \
+	NO_API ATGPPairProjectGameModeBase(const ATGPPairProjectGameModeBase&); \
+public:
+
+
+#define TGPPairProject_Source_TGPPairProject_TGPPairProjectGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATGPPairProjectGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATGPPairProjectGameModeBase(ATGPPairProjectGameModeBase&&); \
+	NO_API ATGPPairProjectGameModeBase(const ATGPPairProjectGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATGPPairProjectGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATGPPairProjectGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATGPPairProjectGameModeBase)
+
+
+#define TGPPairProject_Source_TGPPairProject_TGPPairProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define TGPPairProject_Source_TGPPairProject_TGPPairProjectGameModeBase_h_12_PROLOG
+#define TGPPairProject_Source_TGPPairProject_TGPPairProjectGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TGPPairProject_Source_TGPPairProject_TGPPairProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	TGPPairProject_Source_TGPPairProject_TGPPairProjectGameModeBase_h_15_RPC_WRAPPERS \
+	TGPPairProject_Source_TGPPairProject_TGPPairProjectGameModeBase_h_15_INCLASS \
+	TGPPairProject_Source_TGPPairProject_TGPPairProjectGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define TGPPairProject_Source_TGPPairProject_TGPPairProjectGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TGPPairProject_Source_TGPPairProject_TGPPairProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	TGPPairProject_Source_TGPPairProject_TGPPairProjectGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	TGPPairProject_Source_TGPPairProject_TGPPairProjectGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	TGPPairProject_Source_TGPPairProject_TGPPairProjectGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID TGPPairProject_Source_TGPPairProject_TGPPairProjectGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
