8f61a9a6a7f30d602236af11eaacc2788f8b9f95
OverAHill <33007028+OverAHill@users.noreply.github.com>
2018-11-19 17:11:15 +0000

    Removed Unreal Project
    -see above

--- PairProject/Intermediate/Build/Win64/PairProjectEditor/Development/Engine/SharedPCH.Engine.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/h017026h/TGPPairProject/PairProject/Intermediate/Build/Win64/PairProjectEditor/Development/Engine/SharedPCH.Engine.h"
--- PairProject/Source/PairProject/PairProject.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "PairProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PairProject, "PairProject" );
