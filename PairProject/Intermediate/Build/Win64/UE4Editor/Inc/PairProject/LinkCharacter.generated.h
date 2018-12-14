693a36908e60c2d6630b8466d45c755407a362a3
Angharad Hill <h017026h@student.staffs.ac.uk>
2018-12-14 21:47:25 +0000

    Camera Switch Main
    -bp camera switch on this branch

---
+++ PairProject/Intermediate/Build/Win64/UE4Editor/Inc/PairProject/LinkCharacter.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef PAIRPROJECT_LinkCharacter_generated_h
+#error "LinkCharacter.generated.h already included, missing '#pragma once' in LinkCharacter.h"
+#endif
+#define PAIRPROJECT_LinkCharacter_generated_h
+
+#define PairProject_Source_PairProject_Public_LinkCharacter_h_15_RPC_WRAPPERS
+#define PairProject_Source_PairProject_Public_LinkCharacter_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define PairProject_Source_PairProject_Public_LinkCharacter_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesALinkCharacter(); \
+	friend struct Z_Construct_UClass_ALinkCharacter_Statics; \
+public: \
+	DECLARE_CLASS(ALinkCharacter, ABaseCharacter, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/PairProject"), NO_API) \
+	DECLARE_SERIALIZER(ALinkCharacter)
+
+
+#define PairProject_Source_PairProject_Public_LinkCharacter_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesALinkCharacter(); \
+	friend struct Z_Construct_UClass_ALinkCharacter_Statics; \
+public: \
+	DECLARE_CLASS(ALinkCharacter, ABaseCharacter, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/PairProject"), NO_API) \
+	DECLARE_SERIALIZER(ALinkCharacter)
+
+
+#define PairProject_Source_PairProject_Public_LinkCharacter_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ALinkCharacter(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ALinkCharacter) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ALinkCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ALinkCharacter); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ALinkCharacter(ALinkCharacter&&); \
+	NO_API ALinkCharacter(const ALinkCharacter&); \
+public:
+
+
+#define PairProject_Source_PairProject_Public_LinkCharacter_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ALinkCharacter() { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ALinkCharacter(ALinkCharacter&&); \
+	NO_API ALinkCharacter(const ALinkCharacter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ALinkCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ALinkCharacter); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ALinkCharacter)
+
+
+#define PairProject_Source_PairProject_Public_LinkCharacter_h_15_PRIVATE_PROPERTY_OFFSET
+#define PairProject_Source_PairProject_Public_LinkCharacter_h_12_PROLOG
+#define PairProject_Source_PairProject_Public_LinkCharacter_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	PairProject_Source_PairProject_Public_LinkCharacter_h_15_PRIVATE_PROPERTY_OFFSET \
+	PairProject_Source_PairProject_Public_LinkCharacter_h_15_RPC_WRAPPERS \
+	PairProject_Source_PairProject_Public_LinkCharacter_h_15_INCLASS \
+	PairProject_Source_PairProject_Public_LinkCharacter_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define PairProject_Source_PairProject_Public_LinkCharacter_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	PairProject_Source_PairProject_Public_LinkCharacter_h_15_PRIVATE_PROPERTY_OFFSET \
+	PairProject_Source_PairProject_Public_LinkCharacter_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	PairProject_Source_PairProject_Public_LinkCharacter_h_15_INCLASS_NO_PURE_DECLS \
+	PairProject_Source_PairProject_Public_LinkCharacter_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID PairProject_Source_PairProject_Public_LinkCharacter_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
--- PairProject/Intermediate/Build/Win64/UE4Editor/Inc/PairProject/PairProject.init.gen.cpp
+++ PairProject/Intermediate/Build/Win64/UE4Editor/Inc/PairProject/PairProject.init.gen.cpp
@@ -19,8 +19,8 @@ void EmptyLinkFunctionForGeneratedCodePairProject_init() {}
 			static const UE4CodeGen_Private::FPackageParams PackageParams = {
 				"/Script/PairProject",
 				PKG_CompiledIn | 0x00000000,
-				0xEF2B6559,
-				0x3291CC96,
+				0xD420D11F,
+				0x34FB9E5D,
 				nullptr, 0,
 				METADATA_PARAMS(nullptr, 0)
 			};
