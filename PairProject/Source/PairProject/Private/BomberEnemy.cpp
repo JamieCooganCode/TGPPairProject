e0d6a1f9ee43d6a4bbbce1a7ad49ebc5baafd4e7
Angharad Hill <h017026h@student.staffs.ac.uk>
2019-01-06 19:15:51 +0000

    Check
    -updated sword actors to damage new enemies

--- PairProject/Source/PairProject/Private/BomberEnemy.cpp
+++ PairProject/Source/PairProject/Private/BomberEnemy.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
+#include "BomberEnemy.h"
 #include "PairProject/Public/IAttackable.h"
 #include "PairProject/Public/BaseAttack.h"
-#include "BomberEnemy.h"
+
 
 ABomberEnemy::ABomberEnemy() : ABaseEnemy()
 {
